2d0a6940fc75428b051bdf70aece8ded5685241b
Karl Murray <karl@karlm.dev>
2025-03-15 17:51:37 -0500

    Generate the Advisor Filament Resource

---
+++ app/Filament/Resources/AdvisorResource/Pages/CreateAdvisor.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\AdvisorResource\Pages;
+
+use App\Filament\Resources\AdvisorResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateAdvisor extends CreateRecord
+{
+    protected static string $resource = AdvisorResource::class;
+}
