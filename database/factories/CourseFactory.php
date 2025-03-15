8b815c07f7db69f7e5dbd0f7f58c4deb022a481d
Karl Murray <karl@karlm.dev>
2025-03-15 16:53:28 -0500

    Update the CourseFactory

--- database/factories/CourseFactory.php
+++ database/factories/CourseFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Course>
@@ -17,7 +18,9 @@ class CourseFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->words(fake()->numberBetween(2, 5), true),
+            // Create a fake course title with a random amount of words (between 2 and 5 words),
+            // and capitalize them to make them look like real course titles.
+            'name' => Str::title(fake()->words(fake()->numberBetween(2, 5), true)),
         ];
     }
 }
