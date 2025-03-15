c9922ace8b11ede5d19a51c5d2bcf9891cc05649
Karl Murray <karl@karlm.dev>
2025-03-15 16:47:41 -0500

    Fix UUIDs / Add Relationship for Students

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -2,18 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Spatie\MediaLibrary\MediaCollections\Models\Concerns\HasUuid;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Course extends Model
 {
     /** @use HasFactory<\Database\Factories\CourseFactory> */
-    use HasFactory, HasUuid;
+    use HasFactory, HasUuids;
 
     protected $guarded = [
         'id',
         'created_at',
         'updated_at',
     ];
+
+    /** @return BelongsToMany<Student, $this> */
+    public function students(): BelongsToMany
+    {
+        return $this->belongsToMany(Student::class);
+    }
 }
