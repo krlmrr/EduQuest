465a1eafd9c70312655b29745d60cca432c69ae5
Karl Murray <karl@karlm.dev>
2025-03-15 22:23:56 -0500

    Remove uneeded files.

---
+++ resources/views/components/containers/card.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <p>Card Here</p>
+</div>
---
+++ resources/views/livewire/students/index.blade.php
@@ -0,0 +1,13 @@
+<?php
+use function Livewire\Volt\{state, title};
+
+title(fn() => 'Students | EduQuest');
+state('students');
+
+?>
+
+<div>
+    <x-containers.card>
+
+    </x-containers.card>
+</div>
